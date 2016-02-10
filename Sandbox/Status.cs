9c59b6b0746cb7784cdf5f8c5da7006a088438b3
Toke Frostholm Rasmussen <toke1011@edu.easj.dk>
2016-02-10 21:37:24 +0100

    Identified what status.cs contains

--- Sandbox/Status.cs
+++ Sandbox/Status.cs
@@ -10,12 +10,14 @@ namespace Sandbox
     // can then "like" or "dislike" the status post 
     public class Status
     {
+        // Instance fields
         private string statusText;
         private int likes;
         private int dislikes;
 
         // Create the Status - initially, the status has zero likes
         // and zero dislikes
+        // Constructor
         public Status(string statusText)
         {
             this.statusText = statusText;
@@ -23,6 +25,7 @@ namespace Sandbox
             this.dislikes = 0;
         }
 
+        // Mutators (Methods that change value of instance fields
         public void AddOneLike()
         {
             likes = likes + 1;
@@ -33,6 +36,7 @@ namespace Sandbox
             dislikes = dislikes + 1;
         }
 
+        //Accessors (Methods that return information)
         public string GetStatusText()
         {
             return statusText;
